e9cb0a7e9056c3bb708d0d07187a9c70d07eb096
Xiao-Feng Xie <xie@wiomax.com>
2017-11-26 21:51:42 -0500

    add myprojects/

---
+++ myprojects/examples.bat
@@ -0,0 +1,1 @@
+java -cp ../release/MAOS_BIN.jar maosKernel.MAOSExecuter QKP:Problem=jeu_100_025_01 N=500 T=500 DUP_TIMES=10 solver=STD_NLDUX:nItemTour=2
---
+++ myprojects/setting/QKP/solver/STD_NLDUX.R
@@ -0,0 +1,18 @@
+-P nItemTour < StateRefiner: nItemTour
+-P pMix < R_XS:pMix
+
+-D CostAssigner < implement.QKP.behavior.assign.RelativeRpwClueAssigner
+
+-L StateRepairer < LS_REFINER@BinaryRepairerModules: nItemTour=2, CostAssigner=&CostAssigner
+
+-L R_SS < RSS_Rand@KernelSSModules
+
+-D R_INI < maosKernel.behavior.scratch.LinkedScratchSearch: scratchSearch=&R_SS, incumbentSearch=&StateRepairer
+
+-L R_T < RT_BETTER@KernelRTModules
+
+-D R_XS < implement.common.behavior.combine.UniformXRecombinator: pMix=0.5
+
+-L R_G_O < RG_RSP_XS@KernelRGModules: R_XS=&R_XS
+
+-D R_G < maosKernel.behavior.generate.LinkedMGenerator: baseGenerator=&R_G_O, incumbentSearch=&StateRepairer
